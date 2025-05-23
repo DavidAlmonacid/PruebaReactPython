ddfa7ef5c02f2e17db436c3f44b651460535c2b7
DavidAlmonacid <65629741+DavidAlmonacid@users.noreply.github.com>
2025-05-23 04:28:36 +0000

    Add post-create script to install Microsoft ODBC Driver for SQL Server on Debian

---
+++ post-create.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if ! [[ "11 12" == *"$(grep VERSION_ID /etc/os-release | cut -d '"' -f 2 | cut -d '.' -f 1)"* ]];
+then
+    echo "Debian $(grep VERSION_ID /etc/os-release | cut -d '"' -f 2 | cut -d '.' -f 1) is not currently supported.";
+    exit;
+fi
+
+# Download the package to configure the Microsoft repo
+curl -sSL -O https://packages.microsoft.com/config/debian/$(grep VERSION_ID /etc/os-release | cut -d '"' -f 2 | cut -d '.' -f 1)/packages-microsoft-prod.deb
+# Install the package
+sudo dpkg -i packages-microsoft-prod.deb
+# Delete the file
+rm packages-microsoft-prod.deb
+
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
