8fee20dad8dc1ee587e28279657c48e312e54a44
DavidAlmonacid <65629741+DavidAlmonacid@users.noreply.github.com>
2025-05-23 18:37:15 +0000

    Implement device creation endpoint with error handling and ID retrieval

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,7 @@
-from typing import Union
-
-from fastapi import FastAPI
+import pyodbc
+from fastapi import FastAPI, HTTPException
 from helpers.parse_datetime import parse_datetime
-from models.models import Device
+from models.models import Device, DeviceCreate
 
 from database.database import get_db_connection
 
@@ -61,8 +60,59 @@ async def get_devices() -> list[Device]:
     return devices
 
 
-@app.get("/items/{item_id}")
-def read_item(
-    item_id: int, q: Union[str, None] = None
-) -> dict[str, Union[int, str, None]]:
-    return {"item_id": item_id, "q": q}
+@app.post("/api/v1/devices", response_model=Device, status_code=201)
+async def create_device(device: DeviceCreate):
+    conn = get_db_connection()
+
+    if conn is None:
+        raise RuntimeError("Database connection couldn't be established.")
+
+    try:
+        cursor = conn.cursor()
+        cursor.execute(
+            """
+            INSERT INTO Devices (Manufacturer, Model, ImageUrl, Stock, Price, Description)
+            VALUES (?, ?, ?, ?, ?, ?)
+            """,
+            device.Manufacturer,
+            device.Model,
+            device.ImageUrl,
+            device.Stock,
+            device.Price,
+            device.Description,
+        )
+        conn.commit()
+
+        # Get the ID of the newly inserted record
+        cursor.execute(
+            """
+            SELECT
+                Id,
+                CAST(CreatedAt AS VARCHAR(33)) AS CreatedAt,
+                CAST(UpdatedAt AS VARCHAR(33)) AS UpdatedAt
+            FROM Devices
+            WHERE Model = ?
+            """,
+            device.Model,
+        )
+
+        id_row = cursor.fetchone()
+        if id_row is None or id_row[0] is None:
+            raise RuntimeError("Failed to retrieve the new device ID after insertion.")
+
+        # new_id: int = id_row[0]
+        # created_at = parse_datetime(id_row[1])
+        # updated_at = parse_datetime(id_row[2])
+
+        return Device(
+            Id=id_row[0],
+            CreatedAt=parse_datetime(id_row[1]),
+            UpdatedAt=parse_datetime(id_row[2]),
+            **device.model_dump(),
+        )
+    except pyodbc.IntegrityError:
+        raise HTTPException(status_code=400, detail="Device already exists.")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to create device: {e}")
+    finally:
+        conn.close()
